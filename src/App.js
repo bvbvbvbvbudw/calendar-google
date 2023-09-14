d7f64b0ffff0e46ad53c85371e8c7f68addf5ce0
Bogdan Bikbulatov <batmansila1234@gmail.com>
2023-09-15 00:11:38 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import {GoogleLogin} from 'react-google-login';
 
 function App() {
   return (
     <div className="App">
-        <GoogleLogin clientId={'925413843938-hsl81da43sc7c25v0mrqqh4ic4v97mg8.apps.googleusercontent.com'}/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
