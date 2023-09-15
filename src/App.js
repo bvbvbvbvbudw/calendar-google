c1dab768e050e854acaf8639bab632b3de2072cf
Bogdan Bikbulatov <batmansila1234@gmail.com>
2023-09-15 08:56:13 +0300

    d

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { gapi } from 'gapi-script';
 
 function App() {
     const [events, setEvents] = useState([]);
-
+    const [api, setApi] = useState()
     const loadEvents = () => {
         gapi.client.calendar.events.list({
             calendarId: 'primary', // 'primary' обычно ссылается на календарь текущего пользователя
@@ -24,7 +24,7 @@ function App() {
         // Загружаем библиотеку gapi после аутентификации
         gapi.load('client:auth2', () => {
             gapi.client.init({
-                apiKey: 'AIzaSyBr1i2szcRo1NQ6ETowmlGueloqq35eH3M',
+                apiKey: api,
                 clientId: '524450418250-1vg717h32a9dfob8a4eeq4a7lsbonihm.apps.googleusercontent.com',
                 discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
                 scope: 'https://www.googleapis.com/auth/calendar.readonly',
@@ -41,6 +41,7 @@ function App() {
                 <GoogleLogin
                     onSuccess={(credentialResponse) => {
                         console.log(credentialResponse);
+                        setApi(credentialResponse.credential)
                         loadEvents(); // Загружаем события при успешной аутентификации
                     }}
                     onError={() => {
