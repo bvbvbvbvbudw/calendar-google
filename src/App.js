7f3a06525a8135699c51854eb8b47c27d01dd5c5
Bogdan Bikbulatov <batmansila1234@gmail.com>
2023-09-15 08:44:01 +0300

    d

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import React from 'react';
-import {GoogleOAuthProvider, GoogleLogin} from '@react-oauth/google'
+import React, { useState, useEffect } from 'react';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import { gapi } from 'gapi-script';
 
 function App() {
+    const [events, setEvents] = useState([]);
 
+    const loadEvents = () => {
+        gapi.client.calendar.events.list({
+            calendarId: 'primary', // 'primary' обычно ссылается на календарь текущего пользователя
+            timeMin: new Date().toISOString(),
+            showDeleted: false,
+            singleEvents: true,
+            maxResults: 10,
+            orderBy: 'startTime',
+        }).then(response => {
+            const events = response.result.items;
+            setEvents(events);
+        });
+    };
 
+    useEffect(() => {
+        // Загружаем библиотеку gapi после аутентификации
+        gapi.load('client:auth2', () => {
+            gapi.client.init({
+                apiKey: 'YOUR_API_KEY',
+                clientId: 'YOUR_CLIENT_ID',
+                discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
+                scope: 'https://www.googleapis.com/auth/calendar.readonly',
+            }).then(() => {
+                // Авторизуем пользователя и при успешной авторизации загружаем события
+                gapi.auth2.getAuthInstance().signIn().then(loadEvents);
+            });
+        });
+    }, []);
 
     return (
         <div className="App">
             <GoogleOAuthProvider clientId={'524450418250-1emdb2dnk2v95t5a8s5ltefjrhop85i8.apps.googleusercontent.com'}>
                 <GoogleLogin
                     onSuccess={(credentialResponse) => {
-                        console.log(credentialResponse)
+                        console.log(credentialResponse);
+                        loadEvents(); // Загружаем события при успешной аутентификации
                     }}
                     onError={() => {
-                        console.log('login error')
+                        console.log('login error');
                     }}
                 />
             </GoogleOAuthProvider>
+
+            <h2>События:</h2>
+            <ul>
+                {events.map((event) => (
+                    <li key={event.id}>{event.summary}</li>
+                ))}
+            </ul>
         </div>
     );
 }
