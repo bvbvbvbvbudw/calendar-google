746ca2b9ae9f33069d1bc521010c70d4e0a95e7d
Bogdan Bikbulatov <batmansila1234@gmail.com>
2023-09-15 08:47:41 +0300

    d

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
             maxResults: 10,
             orderBy: 'startTime',
         }).then(response => {
+            console.log(response)
             const events = response.result.items;
             setEvents(events);
         });
