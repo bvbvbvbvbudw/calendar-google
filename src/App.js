b001768c3f7fb54340d24f0233b2e516cf008219
Bogdan Bikbulatov <batmansila1234@gmail.com>
2023-09-15 09:06:20 +0300

    d

--- src/App.js
+++ src/App.js
@@ -4,36 +4,34 @@ import { gapi } from 'gapi-script';
 
 function App() {
     const [events, setEvents] = useState([]);
-    const [api, setApi] = useState()
+    const [apiKey, setApiKey] = useState(''); // Добавляем состояние для apiKey
+
     const loadEvents = () => {
         gapi.client.calendar.events.list({
-            calendarId: 'primary', // 'primary' обычно ссылается на календарь текущего пользователя
+            calendarId: 'primary',
             timeMin: new Date().toISOString(),
             showDeleted: false,
             singleEvents: true,
             maxResults: 10,
             orderBy: 'startTime',
         }).then(response => {
-            console.log(response)
             const events = response.result.items;
             setEvents(events);
         });
     };
 
     useEffect(() => {
-        // Загружаем библиотеку gapi после аутентификации
         gapi.load('client:auth2', () => {
             gapi.client.init({
-                apiKey: api,
+                apiKey: apiKey, // Используем apiKey
                 clientId: '524450418250-1vg717h32a9dfob8a4eeq4a7lsbonihm.apps.googleusercontent.com',
                 discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
                 scope: 'https://www.googleapis.com/auth/calendar.readonly',
             }).then(() => {
-                // Авторизуем пользователя и при успешной авторизации загружаем события
                 gapi.auth2.getAuthInstance().signIn().then(loadEvents);
             });
         });
-    }, []);
+    }, [apiKey]); // Добавляем apiKey в зависимости, чтобы перезагружать gapi.client.init при изменении
 
     return (
         <div className="App">
@@ -41,8 +39,7 @@ function App() {
                 <GoogleLogin
                     onSuccess={(credentialResponse) => {
                         console.log(credentialResponse);
-                        setApi(credentialResponse.credential)
-                        loadEvents(); // Загружаем события при успешной аутентификации
+                        setApiKey(credentialResponse.credential); // Устанавливаем apiKey
                     }}
                     onError={() => {
                         console.log('login error');
