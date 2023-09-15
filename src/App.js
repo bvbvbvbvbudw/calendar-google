23061f03a5809a004d5be46a04674d43bdb7c88e
Bogdan Bikbulatov <batmansila1234@gmail.com>
2023-09-15 09:46:34 +0300

    d

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { gapi } from 'gapi-script';
 
 function App() {
     const [events, setEvents] = useState([]);
-    const [apiKey, setApiKey] = useState(''); // Добавляем состояние для apiKey
 
     const loadEvents = () => {
         gapi.client.calendar.events.list({
@@ -21,17 +20,20 @@ function App() {
     };
 
     useEffect(() => {
-        gapi.load('client:auth2', () => {
-            gapi.client.init({
-                apiKey: 'AIzaSyBmkMhO2LrrXjFcyMp-ROWXzGeBA_ycuw0', // Используем apiKey
-                clientId: '925413843938-hsl81da43sc7c25v0mrqqh4ic4v97mg8.apps.googleusercontent.com',
-                discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
-                scope: 'https://www.googleapis.com/auth/calendar.readonly',
-            }).then(() => {
-                gapi.auth2.getAuthInstance().signIn().then(loadEvents);
+        const test = () => {
+            gapi.load('client:auth2', () => {
+                gapi.client.init({
+                    apiKey: 'AIzaSyBmkMhO2LrrXjFcyMp-ROWXzGeBA_ycuw0', // Используем apiKey
+                    clientId: '925413843938-hsl81da43sc7c25v0mrqqh4ic4v97mg8.apps.googleusercontent.com',
+                    discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
+                    scope: 'https://www.googleapis.com/auth/calendar.readonly',
+                }).then(() => {
+                    gapi.auth2.getAuthInstance().signIn().then(loadEvents);
+                });
             });
-        });
-    }, [apiKey]); // Добавляем apiKey в зависимости, чтобы перезагружать gapi.client.init при изменении
+        }
+        test();
+    }, []); // Добавляем apiKey в зависимости, чтобы перезагружать gapi.client.init при изменении
 
     return (
         <div className="App">
@@ -39,14 +41,14 @@ function App() {
                 <GoogleLogin
                     onSuccess={(credentialResponse) => {
                         console.log(credentialResponse);
-                        setApiKey(credentialResponse.credential); // Устанавливаем apiKey
                     }}
                     onError={() => {
                         console.log('login error');
                     }}
                 />
             </GoogleOAuthProvider>
-
+            <button onClick={loadEvents}>load</button>
+            <button onClick={test}>test</button>
             <h2>События:</h2>
             <ul>
                 {events.map((event) => (
